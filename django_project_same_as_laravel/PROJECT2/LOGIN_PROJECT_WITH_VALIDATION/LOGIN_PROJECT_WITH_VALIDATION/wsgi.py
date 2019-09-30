337a45096c79b00bcd2b6da41f8ed7d846975b24
root <root@pop-os.localdomain>
2019-09-30 19:45:59 +0600

    added basic

---
+++ django_project_same_as_laravel/PROJECT2/LOGIN_PROJECT_WITH_VALIDATION/LOGIN_PROJECT_WITH_VALIDATION/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for LOGIN_PROJECT_WITH_VALIDATION project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LOGIN_PROJECT_WITH_VALIDATION.settings')
+
+application = get_wsgi_application()
